a37e1cf6c805a032a0a2c40c8b865e2d6fa13502
Patrick Muiruri <patrick.muiruri@strathmore.edu>
2024-05-22 19:50:57 +0300

    "Enabling Offline Mode"

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+let cacheData = "appV1"
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=> {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/O.chunk.js',
+                '/static/js/bundle.js',
+                '/index.html',
+                '/'
+            ])
+        })
+    )
+})
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then((resp)=>{
+            if(resp)
+                {
+                    return resp
+                }
+        })
+    )
+})
