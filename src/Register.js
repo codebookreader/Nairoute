87c7d9653779c598792a940843686ad45994d8ae
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2024-06-12 15:35:26 +0300

    Adjustments to home page

--- src/Register.js
+++ src/Register.js
@@ -58,7 +58,7 @@ const Register = () => {
                 <section>
                     <h1>Success!</h1>
                     <p>
-                        <a href="#">Sign In</a>
+                        <a href="/login">Sign In</a>
                     </p>
                 </section>
             ) : (
--- src/homepage.js
+++ src/homepage.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to Nairoute.Need a ride?</h1>
+      <h2>Nairoute is your driver!</h2>
       <a href="/register">Go to Register</a>
+      
     </div>
   );
 }
