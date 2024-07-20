de1de52d725e3e83e55801f64453c87c9f63bf9f
Patrick Muiruri <patrick.muiruri@strathmore.edu>
2024-07-20 18:00:27 +0300

    "Debugging Maps API"

--- src/placesearch.js
+++ src/placesearch.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {LoadScript} from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { LoadScript } from '@react-google-maps/api';
 import Navbar from './Navbar.js';
 import BusRoutes from './BusRoutes.js';
 import GooglePlacesAutocomplete from './GooglePlacesAutocomplete.js';
@@ -7,56 +7,58 @@ import GooglePlacesAutocomplete from './GooglePlacesAutocomplete.js';
 const libraries = ['places'];
 
 const PlaceSearch = () => {
-	const [origin, setOrigin] = useState('');
-	const [destination, setDestination] = useState('');
-	const [startTime, setStartTime] = useState('');
-	const [endTime, setEndTime] = useState('');
-	const [submitted, setSubmitted] = useState(false);
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		setSubmitted(true);
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
 
-	return (
-		<>
-			<Navbar />
-			<LoadScript googleMapsApiKey='' libraries={libraries}>
-				<div className='container mt-5'>
-					<form onSubmit={handleSubmit}>
-						<div className='mb-3'>
-							<GooglePlacesAutocomplete label='Origin' setAddress={setOrigin} />
-						</div>
-						<div className='mb-3'>
-							<GooglePlacesAutocomplete label='Destination' setAddress={setDestination} />
-						</div>
-						<div className='mb-3'>
-							<label className='form-label'>Start Time:</label>
-							<input
-								type='datetime-local'
-								className='form-control'
-								value={startTime}
-								onChange={e => setStartTime(e.target.value)}
-							/>
-						</div>
-						<div className='mb-3'>
-							<label className='form-label'>End Time:</label>
-							<input
-								type='datetime-local'
-								className='form-control'
-								value={endTime}
-								onChange={e => setEndTime(e.target.value)}
-							/>
-						</div>
-						<div className='text-center'>
-							<button type='submit' className='btn btn-primary mt-3'>Get Routes</button>
-						</div>
-					</form>
-					{submitted && <BusRoutes origin={origin} destination={destination} startTime={startTime} endTime={endTime} />}
-				</div>
-			</LoadScript>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <LoadScript googleMapsApiKey='' libraries={libraries}>
+        <div className='container mt-5'>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+              <GooglePlacesAutocomplete label='Origin' setAddress={setOrigin} />
+            </div>
+            <div className='mb-3'>
+              <GooglePlacesAutocomplete label='Destination' setAddress={setDestination} />
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>Start Time:</label>
+              <input
+                type='datetime-local'
+                className='form-control'
+                value={startTime}
+                onChange={(e) => setStartTime(e.target.value)}
+              />
+            </div>
+            <div className='mb-3'>
+              <label className='form-label'>End Time:</label>
+              <input
+                type='datetime-local'
+                className='form-control'
+                value={endTime}
+                onChange={(e) => setEndTime(e.target.value)}
+              />
+            </div>
+            <div className='text-center'>
+              <button type='submit' className='btn btn-primary mt-3'>
+                Get Routes
+              </button>
+            </div>
+          </form>
+          {submitted && <BusRoutes origin={origin} destination={destination} startTime={startTime} endTime={endTime} />}
+        </div>
+      </LoadScript>
+    </>
+  );
 };
 
 export default PlaceSearch;
